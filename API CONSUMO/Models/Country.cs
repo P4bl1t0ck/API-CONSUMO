50dde1ebe1fe96a3fd0ae192a423819a5a3872c3
Pablo.Montalvo <pablo.montalvo.marroquin@udla.edu.ec>
2025-05-14 08:27:53 -0500

    Agregar archivos de proyecto.

---
+++ API CONSUMO/Models/Country.cs
@@ -0,0 +1,13 @@
+﻿namespace API_CONSUMO.Models
+{
+    public class Country
+    {
+        public countryName name { get; set; }
+    }
+    public class  countryName
+    {
+        public string common { get; set; }
+        public string official { get; set; }
+
+    }
+}
